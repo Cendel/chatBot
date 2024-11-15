de1ab8aabc29d18f9c01e340e04a3637466c74d8
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-15 19:16:53 +0100

    fixes chat history issue

--- chatBot.py
+++ chatBot.py
@@ -8,18 +8,18 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Instructions for the AI
 max_tokens, temperature, context = 70, 0, "Data Science"
-message_to_ai = f'''Answer in question's language, 
-                   end with a related emoji,
-                   stick to {context} context
-                   within max {max_tokens} tokens,
-                   10 tokens if out of context.'''
+                   
+system_message = {"role": "system", "content": f'''language same, 
+                                                end with emoji,
+                                                only about: {context}'''}
+
 
 # Load questions and answers
 questions, answers = get_QA_pool()
 
 # Initialize session state
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [{"role": "system", "content": message_to_ai}]
+if "chat_history" not in st.session_state.keys():
+    st.session_state.chat_history  = [{"role": "assistant", "content": f"Ask me a question about {context}!"}]
 
 # Chatbot UI
 st.header("Chat with Techpro Education 🤖")
@@ -30,12 +30,10 @@ if prompt := st.chat_input("Your question:"):
     st.session_state.chat_history.append({"role": "user", "content": prompt})
     answer = get_answer_from_data(prompt, questions, answers)    
     if not answer:
-        answer = get_answer_from_openai(prompt, st.session_state.chat_history, max_tokens, temperature)  
+        answer = get_answer_from_openai(prompt, system_message, max_tokens, temperature)  
     st.session_state.chat_history.append({"role": "assistant", "content": answer})
 
 # Display chat history
 for message in st.session_state.chat_history:
-    if message["role"] == "system":  # skips system messages
-        continue
     with st.chat_message(message["role"]):
         st.write(message["content"])
--- helpers/functions/get_answer_from_openai.py
+++ helpers/functions/get_answer_from_openai.py
@@ -1,16 +1,22 @@
 import openai
                     
-def get_answer_from_openai(prompt, chat_history, max_tokens=70, temperature=0):
-    
+def get_answer_from_openai(prompt,system_message, max_tokens=70, temperature=0):
     
     try:
         response = openai.ChatCompletion.create(
             model="gpt-4",
-            messages=chat_history + [{"role": "user", "content": prompt}],
+            messages = [
+                system_message,
+                {"role": "user", "content": prompt}
+            ],
             max_tokens=max_tokens,
             temperature=temperature
         )
         answer = response['choices'][0]['message']['content'].strip()
         return answer
     except Exception as e:
-        return f"Error: {str(e)}"
+        return "I'm sorry, but I couldn't process your request right now. Please try again later. 🙁"
+
+
+
+
