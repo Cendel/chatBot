72f6ae1fc00e4f2230287d7a07f8dfce35e25eff
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-20 17:55:23 +0100

    Remove image and excel files from gitignore

--- chatBot.py
+++ chatBot.py
@@ -7,12 +7,12 @@ from helpers.functions.get_answer_from_openai import get_answer_from_openai
 openai.api_key = st.secrets["OPENAI_API_KEY"] 
 
 # AI settings
-max_tokens, temperature, context, ai_model = 50, 0, "Data Science", "gpt-3.5-turbo"
+max_tokens, temperature, context, ai_model = 60, 0, "Data Science", "gpt-3.5-turbo"
                    
-system_message = {"role": "system", "content": f''' language same, 
-                                                    end with emoji,
-                                                    only about: {context},
-                                                    max {max_tokens} tokens '''}
+system_message = {"role": "system", "content": f'''full sentences in {max_tokens} tokens!
+                                                   same language, 
+                                                   with emoji,
+                                                   stick to {context}'''}
 
 # Load questions and answers
 questions, answers = get_QA_pool()
@@ -25,7 +25,7 @@ if "chat_history" not in st.session_state.keys():
 st.header("Chat with Techpro Education 🤖")
 
 # 1- gets user input; 2- adds it to chat history; 3- search data for answer; 
-# 4- (optional) asks OpenAI; 5- adds response to chat history: 
+# 4- (optional) asks OpenAI; 5- adds response to chat history:
 if prompt := st.chat_input("Your question:"):
     st.session_state.chat_history.append({"role": "user", "content": prompt})
     answer = get_answer_from_data(prompt, questions, answers)    
